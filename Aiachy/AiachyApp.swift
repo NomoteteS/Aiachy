d85a224c721ee7a9469751de06261544a033754c
NomoteteS <nomotetes.onetrue@icloud.com>
2022-07-27 14:59:09 +0300

    Initial Commit

---
+++ Aiachy/AiachyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiachyApp.swift
+//  Aiachy
+//
+//  Created by NomoteteS on 27.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AiachyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
